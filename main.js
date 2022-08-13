c850a33310a935990bd2a20238cb56078b64b93e
Swiiezzy <105414607+Swiiezzy@users.noreply.github.com>
2022-08-13 12:33:59 +0200

    s

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 const key = "cef19669"  //Api Need this key 
 
 
-
+const movieDisplay = document.getElementById("movie--display")
 const serchValue = document.getElementById("form--serch")
 const serchBtn = document.getElementById("form--btn")
 
+// +++++++++++++++++++++++++++++++++++++++
+// Connect Api and take serched data object
+// +++++++++++++++++++++++++++++++++++++++
 
-serchBtn.addEventListener("click", apiConnect.then)
+serchBtn.addEventListener("click", () => {
+    fetch(`https://www.omdbapi.com/?s=${serchValue.value}&apikey=${key}&plot=full`)
+        .then(res => res.json())
+        .then(data => {
+           
+            serchValue.value = ""
+            movieDisplay.innerHTML = `<p>Jakiś tekst</p>`
+            const serchPool = data.Search.map(movie=>movie.imdbID)
+            console.log(serchPool[0])
+            const serchPoolFetch = serchPool.map(id=>
+              
+                fetch(`http://www.omdbapi.com/?i=${id}&plot=full&apikey=${key}`))
+            console.log(serchPoolFetch[0].Title)
+        })
+})
 
-async function apiConnect() {
-    let response = await fetch(`https://www.omdbapi.com/?s=${serchValue.value}&apikey=${key}`)
-    let data = await response.json()
-    console.log(serchValue.value)
-    console.log(data)
-    return data
-}
+
+
+
+
+
+
+
 
