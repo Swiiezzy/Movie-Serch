5250bb662fef9a7b68c82ddf70ef4dc5d0bce990
Swiiezzy <105414607+Swiiezzy@users.noreply.github.com>
2022-08-13 09:35:49 +0200

    update fetch

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-const key = "778b1943"  //Api Need this key 
+const key = "cef19669"  //Api Need this key 
+
+
+
+const serchValue = document.getElementById("form--serch")
+const serchBtn = document.getElementById("form--btn")
+
+
+serchBtn.addEventListener("click", apiConnect.then)
+
+async function apiConnect() {
+    let response = await fetch(`https://www.omdbapi.com/?s=${serchValue.value}&apikey=${key}`)
+    let data = await response.json()
+    console.log(serchValue.value)
+    console.log(data)
+    return data
+}
 
